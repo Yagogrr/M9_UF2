9c54055d424407f071bc68caaec52d1127a50444
Yago Garcia <yagog5002@gmail.com>
2025-01-15 20:33:26 +0100

    version estable

---
+++ 04-Join/join/src/main/java/iticbcn/m9/Administració.java
@@ -0,0 +1,22 @@
+package iticbcn.m9;
+
+public class Administració {
+    public static final int NUM_POBLACIÓ_ACTIVA = 50;
+    private Treballador[] poblacío_activa = new Treballador[NUM_POBLACIÓ_ACTIVA];
+    public Administració(){
+        for(int i = 0; i < NUM_POBLACIÓ_ACTIVA; i++){
+            poblacío_activa[i] = new Treballador("Ciutadà-"+i, 20, 65, 25000);
+        }
+    }
+    public Treballador[] getTreballadors(){
+        return poblacío_activa;
+    }
+    public static void main(String[] args)throws InterruptedException {
+        Administració admin = new Administració();
+        for(int i = 0;i<NUM_POBLACIÓ_ACTIVA;i++){
+            admin.getTreballadors()[i].start();
+            admin.getTreballadors()[i].join();
+
+        }
+    }
+}
---
+++ 04-Join/join/src/main/java/iticbcn/m9/Treballador.java
@@ -0,0 +1,63 @@
+package iticbcn.m9;
+
+import java.util.Random;
+
+public class Treballador extends Thread {
+
+    private final int NOU_ANUAL_BRUT;
+    private int edat_inici_treball;
+    private int edat_fi_treball;
+    private int edat_actual;
+    private float cobrat;
+    private Random rnd;
+
+    public Treballador(String nom, int edat_inici_treball, int edat_fi_treball, int NOU_ANUAL_BRUT) {
+        super(nom);
+        this.NOU_ANUAL_BRUT = NOU_ANUAL_BRUT;
+        this.edat_inici_treball = edat_inici_treball;
+        this.edat_fi_treball = edat_fi_treball;
+        this.edat_actual = 0;
+        this.cobrat = 0.0f;
+        this.rnd = new Random();
+    } 
+
+    public void cobra() {
+        this.cobrat += NOU_ANUAL_BRUT / 12;
+    }
+
+    public void pagaImpostos(){
+        this.cobrat -= (NOU_ANUAL_BRUT / 12) * 0.24;
+    }
+
+    @Override
+    public void run(){
+        for(int i = edat_inici_treball;i < edat_fi_treball+1;i++){
+            this.edat_actual = i;
+            for(int j = 0;j<12;j++){
+                this.cobra();
+                this.pagaImpostos();
+            }
+            try {
+                Thread.sleep(rnd.nextInt(1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString(){
+        String result = this.getName() + " -> edat: " + this.edat_actual + " / total: " + this.cobrat;
+        return result;
+    }
+
+    public float getCobra(){
+        return this.cobrat;
+    }
+
+    public int getEdat(){
+        return this.edat_actual;
+    }
+   
+}
